0242e29cf485e52139d35b19a48c7aff76a10908
Jennifer Zhang <jennifer.newyork@aol.com>
2020-08-04 23:23:57 +0000

    fixed ssh issue

--- source_control/server_files/register_user.sh
+++ source_control/server_files/register_user.sh
@@ -15,34 +15,8 @@ then
     setfacl -m u:$user:--- /usr/bin/g++
     setfacl -m u:$user:--- /usr/bin/passwd
 
+    #make the .ssh directory so the user can make ssh keys
+    mkdir /home/$user/.ssh
+    sudo chown -R $user /home/$user/.ssh
     echo "Registered user: " $user
-
-else
-	if [ -f $FILE ]
-	then
-	    i=0
-
-	    #reads the first two lines of the file
-	    head -2 $FILE |
-		while read line
-		do
-		    #first line is username
-		    if [ $i -eq 0 ]
-		    then
-			user=$line
-			
-		    #second is the password (bc of scoping issues we have to add user here)
-		    else
-			pass=$line
-			
-			sudo adduser $user --gecos ",,," --disabled-password
-			echo $user":"$pass | sudo chpasswd
-			setfacl -m u:$user:--- /bin/chmod
-			#deletes user_credentials once done
-			rm -rf $FILE
-		    fi
-
-		    ((i=i+1))
-		done
-	fi
 fi
